86b6e41e40d0a54ff0f32921b7e378564954b218
Alexander Seton <alexanderseton@protonmail.com>
2021-11-25 18:37:20 +0000

    Further CSS styling.

--- src/components/Song.js
+++ src/components/Song.js
@@ -10,10 +10,12 @@ const Song = ({song, id}) => {
                 <p><strong>Title: {song["im:name"]["label"]}</strong></p>
                 <p><strong>Artist: {song["im:artist"]["label"]}</strong></p>
                 <a href={song["link"][0]["attributes"]["href"]} target="_blank" rel="noreferrer" className="song-link"><strong>iTunes Link</strong></a>
-                <p><strong>Song Preview:</strong> </p>
-                <audio controls>
-                    <source src={song["link"][1]["attributes"]["href"]} />
-                </audio>
+                <div className="preview-audio">
+                    <p><strong>Song Preview:</strong> </p>
+                    <audio controls>
+                        <source src={song["link"][1]["attributes"]["href"]} />
+                    </audio>
+                </div>
             </div>
             {/* <div className="list-element-image">
                 <img src={song["im:image"][2]["label"]} className="song-image" />
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Song from "./Song.js";
 import "./static/SongList.css";
+import AppleLogo from "../AppleLogo.svg";
 
 const SongList = ({songs}) => {
 
@@ -10,8 +11,14 @@ const SongList = ({songs}) => {
 
     return(
         <div>
-            <h1 id="title">UK Top 20 Songs</h1>
-            <h3 id="sub-title">By iTunes</h3>
+            <nav id="menu-bar">
+                <div id="logo-title">
+                    <img src={AppleLogo} alt="Apple Logo" height="65px" width="65px" id="logo" />
+                    <h1 id="title">Music</h1>
+                </div>
+                <hr />
+            </nav>
+            <h2 id="sub-title">UK Top 20</h2>
             <ul className="song-list">
                 {songItems}
             </ul>
